591e521b5c96a45128a0ac69f8f0274cfd8798d5
Regis <omilale2@gmail.com>
2021-11-23 16:29:54 +0100

    better perspective

--- voxel.py
+++ voxel.py
@@ -74,6 +74,25 @@ p=Point(300,300)
 screen_width,screen_height=800,800
 height, horizon, scale_height= 50, 120, 120
 
+def drawIt():
+    for z in range(distance, 1, -1):
+        # Find line on map. This calculation corresponds to a field of view of 90°
+        pleft  = Point(-z + p.x, -z + p.y)
+        pright = Point( z + p.x, -z + p.y)
+        # segment the line
+        dx = int((pright.x - pleft.x) / screen_width)
+        print(dx)
+        # Raster line and draw a vertical line for each segment
+        for i in range(0, screen_width):
+            height_on_screen = int((int(height) - int(heightRet(pleft.x, pleft.y) )) / z * scale_height + horizon)
+            #print(height_on_screen)
+            #print(i,colorHeight[pleft.x, pleft.y])
+            #DrawVerticalLine(i, height_on_screen, screen_height, colorMap[pleft.x, pleft.y] )#colormap[pleft.x, pleft.y]
+            pygame.draw.lines(screen, colorRet(pleft.x,pleft.y), False, [[i, 800], [i, height_on_screen]], 5)
+            pleft.x += 0.9 #dx
+
+
+
 while not done:
 
     # This limits the while loop to a max of 10 times per second.
@@ -97,29 +116,29 @@ while not done:
                 p.y += 1
     # Clear the screen and set the screen background
     screen.fill(White)
-    pygame.draw.lines(screen, BLACK, False, [[200, 300], [400, 400],[300, 200], [200, 300]], 5)
+    #pygame.draw.lines(screen, BLACK, False, [[200, 300], [400, 400],[300, 200], [200, 300]], 5)
 
 
     # Draw a circle
-    pygame.draw.circle(screen, BLUE, [300, 300], 40)
+    #pygame.draw.circle(screen, BLUE, [300, 300], 40)
     #def Render(p, height, horizon, scale_height, distance, screen_width, screen_height):
     #Render( Point(0, 0), 50, 120, 120, 300, 800, 600 )
     print(p.x,p.y)
-    for z in range(distance, 1, -1):
-        # Find line on map. This calculation corresponds to a field of view of 90°
-        pleft  = Point(-z + p.x, -z + p.y)
-        pright = Point( z + p.x, -z + p.y)
-        # segment the line
-        dx = int((pright.x - pleft.x) / screen_width)
-        # Raster line and draw a vertical line for each segment
-        for i in range(0, screen_width):
-            height_on_screen = int((int(height) - int(heightRet(pleft.x, pleft.y) )) / z * scale_height + horizon)
-            print(height_on_screen)
-            #print(i,colorHeight[pleft.x, pleft.y])
-            #DrawVerticalLine(i, height_on_screen, screen_height, colorMap[pleft.x, pleft.y] )#colormap[pleft.x, pleft.y]
-            pygame.draw.lines(screen, colorRet(pleft.x,pleft.y), False, [[i, 800], [i, height_on_screen]], 5)
-            pleft.x += dx
-
+    # for z in range(distance, 1, -1):
+    #     # Find line on map. This calculation corresponds to a field of view of 90°
+    #     pleft  = Point(-z + p.x, -z + p.y)
+    #     pright = Point( z + p.x, -z + p.y)
+    #     # segment the line
+    #     dx = int((pright.x - pleft.x) / screen_width)
+    #     # Raster line and draw a vertical line for each segment
+    #     for i in range(0, screen_width):
+    #         height_on_screen = int((int(height) - int(heightRet(pleft.x, pleft.y) )) / z * scale_height + horizon)
+    #         print(height_on_screen)
+    #         #print(i,colorHeight[pleft.x, pleft.y])
+    #         #DrawVerticalLine(i, height_on_screen, screen_height, colorMap[pleft.x, pleft.y] )#colormap[pleft.x, pleft.y]
+    #         pygame.draw.lines(screen, colorRet(pleft.x,pleft.y), False, [[i, 800], [i, height_on_screen]], 5)
+    #         pleft.x += dx
+    drawIt()
 
 
 
